ad19c939fa42e2e022965f057f72bb6075e09cd1
Mateusz Pokora <m.pokora@binarapps.com>
2014-10-30 01:41:32 +0100

    [add] ajax create/destroy board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,23 +1,32 @@
+# controller for boards
 class BoardsController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @board = Board.new board_params
-    if @board.save
+    respond_to do |format|
+      return unless @board.save
       BoardConnection.create user_id: current_user.id, board_id: @board.id
-      redirect_to user_dashboard_path, notice: 'Board added successfuly!'
+      format.html do
+        redirect_to user_dashboard_path, notice: 'Board added successfuly!'
+      end
+      format.js { }
     end
   end
 
   def show
-    return if @board = current_user.boards.where(id: params[:id]).first
+    return if @board == current_user.boards.where(id: params[:id]).first
     redirect_to user_dashboard_path, alert: 'You are not allowed to see this board!'
   end
 
   def destroy
     @board = Board.find(params[:id])
     @board.destroy
-    redirect_to user_dashboard_path, notice: 'Board removed'
+    respond_to do |format|
+      format.html { redirect_to user_dashboard_path, notice: 'Board removed' }
+      format.js {}
+    end
+
   end
 
   def update
@@ -32,6 +41,6 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:title, :list_order => [])
+    params.require(:board).permit(:title, list_order: [])
   end
 end
